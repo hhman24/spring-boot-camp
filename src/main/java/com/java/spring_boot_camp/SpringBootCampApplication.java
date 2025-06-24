eab1f06659f5cdb602a707e4a16b4e4c86626120
an.hhm <an.hhm@vgamestd.coman.hhm@vgamestd.com>
2025-06-24 09:33:51 +0700

    feat: init commit

---
+++ src/main/java/com/java/spring_boot_camp/SpringBootCampApplication.java
@@ -0,0 +1,13 @@
+package com.java.spring_boot_camp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCampApplication.class, args);
+	}
+
+}
